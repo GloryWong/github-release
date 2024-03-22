46c694a3388dd5c1b3cab02e6846efd1d258da7c
GloryWong <glorywong1001@gmail.com>
2024-03-23 03:59:21 +0800

    feat: add footer

--- app.vue
+++ app.vue
@@ -12,22 +12,30 @@ useHead({
 <template>
   <div>
     <UContainer class="py-4 sm:py-8">
-      <div>
-        <div class="flex flex-col gap-8">
-          <div
-            class="flex flex-col gap-6 transition ease-in-out duration-1000 z-10" :class="{
-              'translate-y-[calc(50vh-100%)]': initial,
-            }"
-          >
-            <Brand />
-            <Search />
-          </div>
-          <TransitionSlide :offset="[0, '100%']" easing="ease-in-out" :duration="1000">
-            <Result v-if="!initial" />
-          </TransitionSlide>
+      <div class="flex flex-col gap-8">
+        <div
+          class="flex flex-col gap-6 transition ease-in-out duration-1000 z-10" :class="{
+            'translate-y-[calc(50vh-100%)]': initial,
+          }"
+        >
+          <Brand />
+          <Search />
         </div>
+        <TransitionSlide :offset="[0, '100%']" easing="ease-in-out" :duration="1000">
+          <div v-if="!initial" class="flex flex-col gap-2">
+            <Result />
+            <Footer />
+          </div>
+        </TransitionSlide>
       </div>
     </UContainer>
+    <TransitionSlide :offset="[0, '100%']">
+      <div v-if="initial" class="fixed bottom-0 left-0 w-full">
+        <UContainer>
+          <Footer />
+        </UContainer>
+      </div>
+    </TransitionSlide>
     <UNotifications />
   </div>
 </template>
---
+++ components/Footer.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="flex justify-between items-center py-2 sm:py-3 md:py-4 text-gray-500 text-xs sm:text-sm flex-col-reverse sm:flex-row gap-1 sm:gap-0">
+    <p class="text-center">
+      Copyright © 2024 Glory Wong - <a
+        href="https://github.com/GloryWong/github-release/blob/master/LICENSE"
+        target="_blank" class="hover:underline"
+      >MIT
+        License</a>
+    </p>
+    <div class="flex gap-2 items-center text-center">
+      <p>
+        Made with ❤️ by <a href="https://glorywong.com/" target="_blank" class="hover:underline">Glory Wong</a>
+      </p>
+      <a href="https://github.com/GloryWong/github-release" target="_blank" class="text-xl sm:text-2xl hover:text-gray-600 dark:hover:text-gray-400 flex items-center">
+        <Icon name="i-mdi-github" />
+      </a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
