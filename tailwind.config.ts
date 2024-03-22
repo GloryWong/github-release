8b6574376a86c39faf9267357a274e56abe96e83
GloryWong <glorywong1001@gmail.com>
2024-03-22 22:39:07 +0800

    chore: upgrade deps

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,10 @@
 import type { Config } from 'tailwindcss'
-import { scopedPreflightStyles } from 'tailwindcss-scoped-preflight'
+import { isolateOutsideOfContainer, scopedPreflightStyles } from 'tailwindcss-scoped-preflight'
 
 export default <Partial<Config>> {
   plugins: [
     scopedPreflightStyles({
-      cssSelector: '[data-tailwind=false]',
-      mode: 'except matched',
+      isolationStrategy: isolateOutsideOfContainer('[data-tailwind=false]'),
     }),
   ],
 }
